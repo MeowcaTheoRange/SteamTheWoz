e1a52d769f79e7a90e2a8706cecc718491a42b24
MeowcaTheoRange <meowcatheorange@users.noreply.github.com>
2023-03-31 14:21:37 -0500

    DUDE????

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const scott = require("./api/index");
 
 app.use("/api", scott);
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
